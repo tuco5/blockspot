3881eed39f2ccfe9e544c41b85416197e004dbb5
tuco5 <tudor.comparan@gmail.com>
2024-10-25 13:10:36 -0600

    Hotfix: DarkModeToggle

--- src/components/theme/DarkModeToggle.tsx
+++ src/components/theme/DarkModeToggle.tsx
@@ -23,11 +23,11 @@ export function DarkModeToggle() {
     <Toggle
       onPressedChange={toggleDarkMode}
       variant="outline"
-      className="flex w-16 justify-start rounded-full p-1.5 transition-all duration-300 dark:justify-end"
+      className="h-8 w-8 rounded-full p-0"
       pressed={theme ? theme === "dark" : false}
       size={"sm"}
     >
-      <div className="relative flex rounded-full bg-slate-300 p-1.5 dark:bg-slate-200/30">
+      <div className="relative flex rounded-full bg-slate-200 p-2 dark:bg-slate-800/30">
         <Sun className="h-4 w-4 rotate-0 scale-100 transition-all duration-300 dark:absolute dark:-rotate-90 dark:scale-0" />
         <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all duration-300 dark:static dark:rotate-0 dark:scale-100" />
         <span className="sr-only">Toggle theme</span>
