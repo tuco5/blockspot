12e3b672069ce6deda6599c395cde92e30f370c3
tuco5 <tudor.comparan@gmail.com>
2024-10-25 13:04:57 -0600

    Hotfix: footer and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ function Header() {
   return (
     <header className="flex w-full justify-center border-b">
       <div className="dark:primary flex w-full max-w-screen-lg items-center justify-between p-4">
-        <Logo className="text-xl" />
+        <Logo className="text-2xl" />
         <div className="flex items-center gap-2">
           <DarkModeToggle />
           <LocaleSwitcher />
