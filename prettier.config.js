10ce145ea05331602d2490cc6b9603eea3adaede
tuco5 <tudor.comparan@gmail.com>
2024-10-29 16:44:16 -0600

    more styling changes

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
-  printWidth: 120,
+  printWidth: 80,
   tabWidth: 2,
   trailingComma: "all",
   bracketSpacing: true,
