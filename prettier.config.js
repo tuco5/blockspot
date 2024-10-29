346686f3e27dbf46b6f27c997ec712e7581fff15
tuco5 <tudor.comparan@gmail.com>
2024-10-29 16:19:55 -0600

    add prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 120,
+  tabWidth: 2,
+  trailingComma: "all",
+  bracketSpacing: true,
 };
 
 export default config;
