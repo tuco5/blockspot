ad1b1189aa3e1a935ff4667437b21643950dbd53
tuco5 <tudor.comparan@gmail.com>
2024-10-31 10:48:26 -0600

    confirm page complete

--- src/app/auth/confirm/page.tsx
+++ src/app/auth/confirm/page.tsx
@@ -1,9 +1,14 @@
 import { PageTemplate, Header } from "@/components/template";
+import { useTranslations } from "next-intl";
 export default function ConfirmPage() {
+  const t = useTranslations("ConfirmPage");
   return (
     <PageTemplate>
       <Header />
-      Confirm page
+      <main className="flex h-[50vh] flex-col items-center justify-center gap-2">
+        <h1 className="text-center text-4xl text-primary">{t("title")}</h1>
+        <p className="text-center text-2xl">{t("subtitle")}</p>
+      </main>
     </PageTemplate>
   );
 }
