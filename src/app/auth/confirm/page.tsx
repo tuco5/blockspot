373dffb083e4e7ccfae52d8ea6af4ccfc89cfd26
tuco5 <tudor.comparan@gmail.com>
2024-10-31 10:29:37 -0600

    confirm email page skaffold

---
+++ src/app/auth/confirm/page.tsx
@@ -0,0 +1,9 @@
+import { PageTemplate, Header } from "@/components/template";
+export default function ConfirmPage() {
+  return (
+    <PageTemplate>
+      <Header />
+      Confirm page
+    </PageTemplate>
+  );
+}
