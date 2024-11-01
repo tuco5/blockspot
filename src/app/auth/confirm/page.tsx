c98f22e4899a02af11ca6004e0c9c4787a029055
tuco5 <tudor.comparan@gmail.com>
2024-11-01 11:19:47 -0600

    confirm page bigger font

--- src/app/auth/confirm/page.tsx
+++ src/app/auth/confirm/page.tsx
@@ -3,7 +3,9 @@ export default function ConfirmPage() {
   const t = useTranslations("ConfirmPage");
   return (
     <main className="flex h-[50vh] flex-col items-center justify-center gap-2">
-      <h1 className="text-center text-4xl text-primary">{t("title")}</h1>
+      <h1 className="text-wrap text-center text-5xl font-semibold tracking-tight text-primary">
+        {t("title")}
+      </h1>
       <p className="text-center text-2xl">{t("subtitle")}</p>
     </main>
   );
