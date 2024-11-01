93ab941949955095cc4fda6ba06fa5c0e8fa893b
tuco5 <tudor.comparan@gmail.com>
2024-11-01 12:10:49 -0600

    finish log in and log out flow

--- src/components/common/Avatar.tsx
+++ src/components/common/Avatar.tsx
@@ -1,28 +1,76 @@
-import { createClient } from "@/server/supabase/server";
+"use client";
 import {
   Avatar as AvatarRoot,
   AvatarFallback,
   AvatarImage,
 } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuGroup,
+} from "@/components/ui/dropdown-menu";
+import { createClient } from "@/server/supabase/client";
+import { type User } from "@supabase/supabase-js";
+import { LogOut, Settings, User as UserIcon } from "lucide-react";
+import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
-export async function Avatar() {
-  const supabase = await createClient();
+interface AvatarProps {
+  user: User | null;
+}
+export function Avatar({ user }: AvatarProps) {
+  const t = useTranslations("AvatarMenu");
 
-  const { data, error } = await supabase.auth.getUser();
-
-  if (error) {
-    console.error({ error });
+  if (!user) {
+    return (
+      <Button asChild variant="secondary" rounded="full">
+        <Link href="/auth/sign/in">{t("sign_in")}</Link>
+      </Button>
+    );
   }
 
-  if (!data.user) return null;
-
-  // TODO: Remove this console log
-  console.info({ data });
+  const supabase = createClient();
+  const router = useRouter();
+  const Logout = () => {
+    supabase.auth
+      .signOut()
+      .then(() => router.refresh())
+      .catch(console.error);
+  };
 
   return (
-    <AvatarRoot>
-      <AvatarImage src={data.user.user_metadata.avatar_url} />
-      <AvatarFallback>{data.user.email?.[0]?.toUpperCase()}</AvatarFallback>
-    </AvatarRoot>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <AvatarRoot className="cursor-pointer">
+          <AvatarImage src={user?.user_metadata?.avatar_url} />
+          <AvatarFallback>{user?.email?.[0]?.toUpperCase()}</AvatarFallback>
+        </AvatarRoot>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40">
+        <DropdownMenuLabel>{t("account")}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <UserIcon />
+            <span>{t("profile")}</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Settings />
+            <span>{t("settings")}</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="text-destructive" onSelect={Logout}>
+          <LogOut />
+          <span>{t("logout")}</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,15 +1,20 @@
+import { createClient } from "@/server/supabase/server";
 import { LocaleSwitcher } from "@/components/locale";
 import { DarkModeToggle } from "@/components/theme/DarkModeToggle";
 import { Logo, Avatar } from "@/components/common";
 
-export function Header() {
+export async function Header() {
+  const supabase = await createClient();
+
+  const { data } = await supabase.auth.getUser();
+
   return (
     <header className="flex w-full justify-center border-b dark:bg-black/30">
       <div className="dark:primary flex w-full max-w-screen-lg items-center justify-between p-4">
         <Logo className="text-2xl" />
         <div className="flex items-center gap-2">
           <div className="mr-2 border-r pr-4">
-            <Avatar />
+            <Avatar user={data.user} />
           </div>
           <DarkModeToggle />
           <LocaleSwitcher />
