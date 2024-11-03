3d4301aee72d7a1a638d717f3d9ef2c4f0bbbc79
tuco5 <tudor.comparan@gmail.com>
2024-11-03 09:09:39 -0600

    fix hydratation error in custom hook

--- src/hooks/useBreakPoints.ts
+++ src/hooks/useBreakPoints.ts
@@ -1,14 +1,18 @@
+import { useEffect, useState } from "react";
 import { useWindowSize } from "usehooks-ts";
 
 export type BP = "sm" | "md" | "lg" | "xl" | "2xl";
 export default function useBreakPoints(bp: BP): boolean {
-  const { width = 0, height = 0 } = useWindowSize();
+  const { width = 0 } = useWindowSize();
+  const [isResponsive, setIsResponsive] = useState(false);
 
-  if (bp === "sm") return width < 640;
-  if (bp === "md") return width < 768;
-  if (bp === "lg") return width < 1024;
-  if (bp === "xl") return width < 1280;
-  if (bp === "2xl") return width < 1536;
+  useEffect(() => {
+    if (bp === "2xl") setIsResponsive(width < 1536);
+    if (bp === "xl") setIsResponsive(width < 1280);
+    if (bp === "lg") setIsResponsive(width < 1024);
+    if (bp === "md") setIsResponsive(width < 768);
+    if (bp === "sm") setIsResponsive(width < 640);
+  }, []);
 
-  return false;
+  return isResponsive;
 }
