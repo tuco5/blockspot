f499d6daacfa9b67725e542cf618f71937e26ada
tuco5 <tudor.comparan@gmail.com>
2024-11-02 22:32:42 -0600

    add href prop to logo component

--- src/components/template/Logo.tsx
+++ src/components/template/Logo.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 
-export function Logo({ className }: Props) {
+interface LogoProps extends Props {
+  href?: string;
+}
+export function Logo({ className, href = "/" }: LogoProps) {
   return (
-    <Link href="/" className={cn("font-semibold", className)}>
+    <Link href={href} className={cn("font-semibold", className)}>
       block<span className="text-orange-500 dark:text-rose-500">spot</span>
     </Link>
   );
