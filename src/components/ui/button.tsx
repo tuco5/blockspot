6034d2bb0e3e0e9bc0badd99eeea2976175f81b4
tuco5 <tudor.comparan@gmail.com>
2024-11-04 14:44:25 -0600

    modify ui components style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,6 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-white-foreground shadow hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
@@ -36,8 +35,9 @@ const buttonVariants = cva(
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "primary",
       size: "default",
+      rounded: "full",
     },
   },
 );
