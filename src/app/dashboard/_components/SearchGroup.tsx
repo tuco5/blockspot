bbc65ba8a2c739022c9cca9bb440b9ff383b99ed
tuco5 <tudor.comparan@gmail.com>
2024-11-06 09:36:50 -0600

    search input clearable, default value search params

--- src/app/dashboard/_components/SearchGroup.tsx
+++ src/app/dashboard/_components/SearchGroup.tsx
@@ -1,23 +1,50 @@
 "use client";
 import Form from "next/form";
-import { Search } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import { Search, X } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRef, useState } from "react";
 
 export function SearchGroup({ className }: Props) {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search");
+
+  const [searchInput, setSearchInput] = useState(search ?? "");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  function handleClear() {
+    setSearchInput("");
+    inputRef.current?.focus();
+  }
+
   return (
     <Form
       className={cn("relative w-full max-w-80", className)}
       action="/dashboard/groups"
     >
+      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
       <Input
+        ref={inputRef}
         id="search"
         name="search"
-        placeholder="Buscar un grupo..."
         type="search"
+        placeholder="Buscar un grupo..."
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
         className="rounded-full bg-card pl-8"
       />
-      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
+      {searchInput ? (
+        <Button
+          variant="ghost"
+          className="absolute right-3 top-1/2 flex h-6 w-6 -translate-y-1/2 items-center justify-center bg-accent p-0"
+          type="button"
+          onClick={handleClear}
+        >
+          <X className="h-4 w-4" />
+        </Button>
+      ) : null}
     </Form>
   );
 }
