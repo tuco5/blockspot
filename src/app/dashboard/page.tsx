0493daa2de2ae3133f56d7ccb43a44e24dcf0c9c
tuco5 <tudor.comparan@gmail.com>
2024-11-06 11:33:12 -0600

    redesign dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
-import Image from "next/image";
-import { ImageIcon, Plus } from "lucide-react";
+import { MoveRight, Plus } from "lucide-react";
 import { createClient } from "@/server/supabase/server";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
-import { Title } from "@/components/template";
+import { GroupsList } from "@/components/template";
 import { db } from "@/server/db";
-import { Group } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { SearchGroup } from "./_components/SearchGroup";
 
@@ -22,82 +20,31 @@ export default async function DashboardPage() {
   });
 
   return (
-    <main className="flex w-full max-w-screen-lg flex-col items-center gap-8 p-2">
-      <Title className="mt-6">¡Hola {user?.name}!</Title>
-
+    <main className="flex w-full max-w-screen-lg flex-col items-center gap-6 px-2 py-6">
       <div className="flex w-full max-w-screen-md justify-between gap-8">
-        <h2 className="text-2xl font-semibold">Mis Grupos</h2>
-        <SearchGroup />
         <Link
           href="/dashboard/groups/new"
-          className={cn(buttonVariants({ variant: "primary" }))}
+          className={cn(buttonVariants({ variant: "primary", rounded: "md" }))}
         >
           <Plus />
-          <span>Crear</span>
+          <span>Hub Nuevo</span>
+        </Link>
+        <Link
+          href="/dashboard/groups"
+          className={cn(
+            buttonVariants({ variant: "secondary", rounded: "md" }),
+          )}
+        >
+          <span>Unirme a un Hub</span>
+          <MoveRight />
         </Link>
       </div>
 
+      <SearchGroup />
       <GroupsList
         groups={user?.ownerOf ?? []}
         emptyMsg="No has creado ningun grupo aún."
       />
     </main>
-  );
-}
-
-interface GroupsListProps {
-  groups: Group[];
-  emptyMsg: string;
-}
-function GroupsList({ groups, emptyMsg }: GroupsListProps) {
-  if (groups.length === 0) {
-    return (
-      <p className="text-center italic text-slate-500 dark:text-slate-300">
-        {emptyMsg}
-      </p>
-    );
-  }
-
-  return (
-    <div className="flex w-full flex-wrap items-center justify-center gap-4">
-      {groups.map((group) => (
-        <GroupCard key={group.id} {...group} />
-      ))}
-    </div>
-  );
-}
-
-type GroupCardProps = Group;
-function GroupCard({ name, image, id }: GroupCardProps) {
-  const imgSize = "h-48 w-48";
-  return (
-    <Link
-      href={`/groups/${id}`}
-      className={cn("relative flex overflow-hidden rounded-lg", imgSize)}
-    >
-      {image ? (
-        <Image
-          src={image}
-          alt={name}
-          height={200}
-          width={200}
-          className={cn("absolute inset-0 object-cover", imgSize)}
-        />
-      ) : (
-        <div
-          className={cn(
-            "absolute inset-0 flex items-center justify-center rounded-lg bg-slate-400/30",
-            imgSize,
-          )}
-        >
-          <ImageIcon className="h-24 w-24 text-slate-500 dark:text-slate-300" />
-        </div>
-      )}
-      <div className="z-10 flex h-full w-full flex-col items-center text-white">
-        <h3 className="w-full bg-black/60 px-1 py-2 text-center text-lg font-bold">
-          {name}
-        </h3>
-      </div>
-    </Link>
   );
 }
