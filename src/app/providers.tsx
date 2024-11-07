95a857ad9de142f7e076f2d864d32d712bf8a259
tuco5 <tudor.comparan@gmail.com>
2024-11-07 15:34:08 -0600

    init usequery in providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,14 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
 
+const queryClient = new QueryClient();
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
