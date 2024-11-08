7cad608e34fed0ddde00f52b3d727eb86fff0c7b
tuco5 <tudor.comparan@gmail.com>
2024-11-07 18:46:02 -0600

    get query params util

---
+++ src/server/utils/getQueryParams.ts
@@ -0,0 +1,10 @@
+export async function getQueryParams(
+  searchParams: SearchParams,
+): Promise<QueryParams> {
+  const sp = await searchParams;
+
+  const take = sp.take && Number.isNaN(sp.take) ? Number(sp.take) : 20;
+  const search = sp.search ?? "";
+
+  return { search, take };
+}
