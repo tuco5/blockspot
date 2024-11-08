864f69dd726ab889ae55eadcd10dfdeee04792ee
tuco5 <tudor.comparan@gmail.com>
2024-11-07 18:47:15 -0600

    search hubs page server component

--- src/app/dashboard/hubs/page.tsx
+++ src/app/dashboard/hubs/page.tsx
@@ -1,36 +1,35 @@
-"use client";
-import { useTranslations } from "next-intl";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { HubsList, Title } from "@/components/template";
+import { Title } from "@/components/template";
 import { SearchForm } from "@/components/forms/SearchForm";
-
-import { useQueryParams } from "@/hooks/useQueryParams";
+import { getTranslations } from "next-intl/server";
+import { SearchHubsList } from "./_components/SearchHubsList";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getQueryParams } from "@/server/utils/getQueryParams";
 import { getHubsAction } from "./actions";
 
-export default function HubsPage() {
-  const t = useTranslations("HubsPage");
+export default async function HubsPage(props: {
+  params: Params;
+  searchParams: SearchParams;
+}) {
+  const t = await getTranslations("HubsPage");
+  const { search, take } = await getQueryParams(props.searchParams);
 
-  const { search, take } = useQueryParams();
-  console.log({ search, take });
-
-  const { data, hasNextPage, fetchNextPage, isFetchingNextPage, error } =
-    useInfiniteQuery({
-      queryKey: ["hubs", { search, take }],
-      queryFn: async ({ pageParam }) =>
-        await getHubsAction({ search, take, cursor: pageParam }),
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-      initialPageParam: "",
-    });
-
-  const hubs = data?.pages.flatMap((page) => page.hubs) || [];
-
-  console.info(data);
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["hubs", { search, take }],
+    queryFn: () => getHubsAction({ search, take }),
+  });
 
   return (
-    <main className="flex w-full max-w-screen-lg flex-col items-center gap-8 p-2">
+    <main className="flex min-h-screen w-full max-w-screen-lg flex-col items-center gap-8 p-2">
       <Title className="mt-6">{t("title")}</Title>
       <SearchForm placeholder={t("search_placeholder")} defaultValue={search} />
-      <HubsList hubs={hubs} />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <SearchHubsList />
+      </HydrationBoundary>
     </main>
   );
 }
