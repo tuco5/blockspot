7bea2b9d77ee6814d0d655a5df29115f26132872
tuco5 <tudor.comparan@gmail.com>
2024-11-07 20:06:45 -0600

    add debounce feature

--- src/app/dashboard/hubs/_components/SearchHubsList.tsx
+++ src/app/dashboard/hubs/_components/SearchHubsList.tsx
@@ -3,23 +3,26 @@ import { use, useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
 import { useInView } from "react-intersection-observer";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { useQueryParams } from "@/hooks/useQueryParams";
 import { PulseLoader } from "react-spinners";
+import { useDebounceCallback } from "usehooks-ts";
+import { useQueryParams } from "@/hooks/useQueryParams";
 import { getHubsAction } from "../actions";
 import { SearchForm } from "@/components/forms";
 import { HubsList } from "@/components/template";
 import { useRouter } from "next/navigation";
 
+const BASE_URL = "/dashboard/hubs";
+
 export function SearchHubsList() {
   const t = useTranslations("HubsPage");
 
   const { search, take } = useQueryParams();
-
   const [input, setInput] = useState(search ?? "");
-
   const router = useRouter();
-
   const { ref, inView } = useInView();
+  const debounce = useDebounceCallback((value) => {
+    router.push(`${BASE_URL}?search=${value}&take=${take}`);
+  }, 200);
 
   const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
     useInfiniteQuery({
@@ -36,15 +39,22 @@ export function SearchHubsList() {
     }
   }, [inView, hasNextPage, fetchNextPage]);
 
+  const onClear = () => {
+    setInput("");
+    router.push(BASE_URL);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+    debounce(e.target.value);
+  };
+
   return (
     <div className="flex h-full w-full flex-col items-center gap-6">
       <SearchForm
-        onClear={() => {
-          setInput("");
-          router.push("/dashboard/hubs");
-        }}
+        onClear={onClear}
         value={input}
-        onChange={(e) => setInput(e.target.value)}
+        onChange={onChange}
         placeholder={t("search_placeholder")}
       />
       <HubsList hubs={data?.pages.flatMap((page) => page.hubs) ?? []} />
